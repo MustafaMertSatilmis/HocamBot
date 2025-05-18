912ecd170ae21a88abf446ad8b6f9f2bfcd57054
MustafaMertSatilmis <70822298+MustafaMertSatilmis@users.noreply.github.com>
2025-05-18 15:24:24 +0300

    Update retrieval_grader.py

--- model/graph/chains/retrieval_grader.py
+++ model/graph/chains/retrieval_grader.py
@@ -17,7 +17,8 @@ class GradeDocuments(BaseModel):
 structured_llm_grader = llm.with_structured_output(GradeDocuments)
 
 system = """You are a grader assessing relevance of a retrieved document to a user question. \n 
-    If the document contains keyword(s) or semantic meaning related to the question, grade it as relevant. \n
+    If the document contains keyword(s) and semantic meaning related to the question, grade it as relevant. 
+    Checking semantic meaning and see if the answer lie in the document is extremly important \n
     Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the question."""
 grade_prompt = ChatPromptTemplate.from_messages(
     [
