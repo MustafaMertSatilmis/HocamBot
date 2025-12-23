b6bb13e1519fd76e90d2453f68c7a3f57b4b5001
Mustafa Mert Satılmış <70822298+MustafaMertSatilmis@users.noreply.github.com>
2025-12-23 14:17:19 +0300

    Uncomment import statement for langchain hub

--- model/graph/chains/generation.py
+++ model/graph/chains/generation.py
@@ -1,4 +1,4 @@
-#from langchain import hub
+from langchain import hub
 from langchain_core.output_parsers import StrOutputParser
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import PromptTemplate
